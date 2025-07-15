2438c5a21ac81b8ae87b4acb259c0a5c939b2192
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2025-07-15 21:56:33 +0200

    Curr

--- src/benjamin_schwerdtner/problems/hiff.clj
+++ src/benjamin_schwerdtner/problems/hiff.clj
@@ -11,12 +11,6 @@
 ;; Modeling building-block interdependency
 ;; https://link.springer.com/chapter/10.1007/BFb0056853
 
-;; 0 0 = 1
-;; 1 0 = 0
-;; 0 1 = 1
-;; 1 1 = 1
-(defn iff [a b]
-  (if (= a b) 1 0))
 
 ;; idea:
 ;; - label 'interpret' sub blocks with transform function
@@ -36,21 +30,20 @@
 
 ;; represents a hierachical block structure
 ;;
-;; k = number of sub-blocks in a block
-;; p = is the number of levels in the hierarchy
-;;
-;; at the bottom, transform each block (consisting of k symbols) to a single
-;; symbol by the transform function `t`.
-;;
+;; k = number of sub-blocks in a block = `block-size`
+;; p = is the number of levels in the hierarchy = `hierarchy-levels`
 ;;
 
-;; - creates a new string with length k^(p-1), (first level decoded)
-;; - repeat for each level in the hierachy to make a single symbol
 ;;
 
 
-;; p: hierachy-levels
-;; k: block-size
+;; 0 0 = 1
+;; 1 0 = 0
+;; 0 1 = 1
+;; 1 1 = 1
+(defn iff [a b]
+  (if (= a b) 1 0))
+
 
 ;;
 ;; T(B) =
@@ -59,6 +52,23 @@
   (partition (/ (count coll) size) coll))
 
 (defn hierachical-transform
+  "
+  In effect, calls `transform` with `block-size` elements to 'interpret' 1 level.
+
+  It goes then up 1 hierachy level and repeats. So `transform` is called on the
+  interpretation of the leaf symbols.
+
+  (given by `block-size` and the length of the input).
+
+  Example with 'iff' as transform:
+
+  (hierachical-transform iff 2 [0 0])
+  1
+  (hierachical-transform iff 2 [0 0 1 1])
+  1
+  (hierachical-transform iff 2 [0 0 1 0])
+  0
+  "
   [transform block-size [elm :as input]]
   (if (= (count input) 1)
     ;; atom level
@@ -73,19 +83,25 @@
 ;; F(B) =
 (defn hierachical-block-fitness
   "
-  f: f(x) -> number, the fitness of a single symbol.
-"
-  [transform f block-size [elm :as input]]
-  (if (= (count input) 1)
-    (f elm)
-    ;; -------
-    (+ (* (count input)
-          (f (hierachical-transform transform block-size input)))
-       (reduce +
-               (map
-                (partial hierachical-block-fitness transform f block-size)
-                (blocks block-size input))))))
+  Returns the hierachical block fitness `F(B)`.
 
+  R. Watson, G. S. Hornby, J. B. Pollack 2006
+  Modeling building-block interdependency
+  https://link.springer.com/chapter/10.1007/BFb0056853
+
+  This is for creating '(inter)dependent' problems, i.e. where
+  the fitness contribution of 1 feature depends on the context of another.
+
+
+  transform: Interprets `block-size` elements into a single symbol,
+  which is input to `f` and `transform`.
+
+  f: f(x) -> number, the fitness of a single symbol.
+
+
+  The outcome of fitness is scaled by the level of hierachy,
+
+  Example:
 
 (def hierachical-if-and-only-if
   (partial
@@ -101,9 +117,61 @@
    2))
 
 
-(comment
-  (hierachical-if-and-only-if [0 0 1 1])
+  Each atomic bit contributes to H-IFF:
 
+  (hierachical-if-and-only-if [0 1])
+  2
+
+  On the scond level of hierachy, the combination of the same bit counts twice:
+
+  (hierachical-if-and-only-if [0 0])
+  4
+
+
+  In a Dawkins like analogy, carnivore teeth count a little bit fitness and herbivore stomach, too.
+  But carnivore teeth in the context of carnivore stomach count even more.
+
+
+"
+  [transform f block-size [elm :as input]]
+  (if (= (count input) 1)
+    (f elm)
+    ;; -------
+    (+ (* (count input)
+          (f (hierachical-transform transform block-size input)))
+       (reduce +
+               (map
+                (partial hierachical-block-fitness transform f block-size)
+                (blocks block-size input))))))
+
+
+
+(def hierachical-if-and-only-if
+  (partial
+   hierachical-block-fitness
+   (fn t [a b]
+     ;; t outcome is ∈ {0,1,nil}
+     ({[0 0] 1 [1 1] 1} [a b]))
+   (fn f [a]
+     ;; 1 -> 1
+     ;; 0 -> 1
+     ;; nil -> 0
+     ({0 1 1 1} a 0))
+   2))
+
+(comment
+  (hierachical-transform iff 2 [0 0])
+  1
+  (hierachical-transform iff 2 [0 0 1 1])
+  1
+  (hierachical-transform iff 2 [0 0 1 0])
+  0
+  (hierachical-if-and-only-if [0 0 1 1])
+  12
+  (hierachical-if-and-only-if [0 0])
+  4
+  (hierachical-if-and-only-if [0 1])
+  2
   (hierachical-if-and-only-if [0 0 0 0 1 1 1 1])
 
   ;; This is basically the canonical function that is hard for hill climbing algorithms
@@ -136,15 +204,8 @@
                        (permute-vec bit-perm input-vec)))}))
 
 
-
-
 (comment
   [(hierachical-transform iff 2 [0 1 1 1])
    (hierachical-transform iff 2 [1 1 1 1])]
   [0 1]
-
-
-
-  (make-shuffled-hiff {:hierachy-levels 2})
-
-  )
+  (make-shuffled-hiff {:hierachy-levels 2}))
