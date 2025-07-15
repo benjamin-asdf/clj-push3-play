2ca27b358111cf335d9b82fba098052fd6c9e333
Benni Schwerdtner <Benjamin.Schwerdtner@gmail.com>
2025-07-15 21:26:07 +0200

    Curr

--- problems/busy_beaver.clj
+++
@@ -1,8 +0,0 @@
-(ns busy-beaver
-  (:require
-   [benjamin-schwerdtner.clj-push3-play.generator :as gen]
-   [benjamin-schwerdtner.clj-push3-play.stack.pushstate :as stack]
-   [benjamin-schwerdtner.clj-push3-play.instructions.interface :as
-    instructions]
-   [benjamin-schwerdtner.clj-push3-play.interpreter :as push]
-   [benjamin-schwerdtner.clj-push3-play.mutation.mut :as mut]))
